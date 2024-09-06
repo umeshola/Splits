9bfdb49e82b1c19bbafc81210e2ad012409a6ac2
UmeshOla <umeshola07@gmail.com>
2024-09-06 14:19:37 +0530

    loading section io

--- client/src/Components/Loading.jsx
+++ client/src/Components/Loading.jsx
@@ -1,8 +1,9 @@
-// Components/Loading.jsx
+import React from 'react';
+
 const Loading = () => {
     return (
-        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-            <h2>Loading... Please wait</h2>
+        <div className="flex items-center justify-center h-screen bg-black">
+            <div className="w-20 h-20 border-t-2 border-red-500 rounded-full animate-spin"></div>
         </div>
     );
 };
